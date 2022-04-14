a1c20c98efb8ef2faf361caa171ea9bc7bf1629a
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-04-14 16:55:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this template
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            .col{
+                background-color: green;
+            } 
+            
+        </style>
+        <!-- <script type="text/javascript" src="script.js"></script> -->
+        
+    </head>
+    <body>
+        <a id="mylink" href="https://karunya.edu">Click me</a><br> 
+        <div id="col">
+            <script type="text/javascript">
+                document.write("Hello World print using javaScript <br> ");
+                face = [['R','G','B'],['Y','J','D'],['y','W','S']]
+                document.write(face[1][2]);
+                document.write("<br>");
+                toys = ['DSA','Java','WebDevelopment','Software Engineering','Crptography','System Software and Compiler design']
+                greeting = "Hello There";
+                warning = "Be careful";
+                document.write(greeting);
+                document.write("<br>");
+                toys = ['DSA','Java','WebDevelopment','Software Engineering','Crptography','System Software and Compiler design'];
+                document.write(toys[0]);
+                document.write("<br>");
+                status = "All Systems are working";
+                document.write(status);
+                document.write("<br>");
+                count = 42;
+                temperature = 98.4;
+                document.write(count+" "+temperature);
+                document.write("<br>");
+                //Assigment Operators
+                j = 99;
+                j += 2;
+                document.write(j);
+                document.write("<br>");
+                j += 'String';
+                document.write(j);
+                n1 = 10; n2 = 10.3;
+                document.write("<br>");
+                if(n1 === n2){
+                    document.write("True");
+                }
+                else {
+                    document.write("False");
+                }
+                document.write("<br>");
+                message = "to send";
+                document.write("You Have " + message + " message");
+                name = "James";
+                name += " Dean";
+                document.write("<br>");
+                document.write(name);
+                headings = "Name\tAge\tLocation";
+                document.write("<br>");
+                document.write(headings);
+                n = 12345 * 67890;
+                document.write("<br>");
+                document.write(n);
+                document.write("<br>");
+                function product(a, b){
+                    return a*b;
+                }
+                document.write(product(5,6));
+                
+                url = document.links.mylink.href
+                document.write('The URL is '+ url);
+                document.write("<br>");
+                
+                document.write("a: " + (42 > 3) + "<br>");
+                document.write("b: " + (91 < 4) + "<br>");
+                document.write("c: " + (8 == 12) + "<br>");
+                document.write("d: " + (4 < 17) + "<br>");
+                
+                g1 = 1; g2 = 0;
+                document.write((g1 & g2) + "<br>");
+                document.write((g1 || g2) + "<br>");
+                document.write((!g2));
+                
+                document.write("<br>");
+                
+                page = "News"
+                
+                if  (page === "Home") document.write("You Selected Home");
+                else if(page === "About") document.write("You selected About");
+                else if(page === "News") document.write("You Selected News");
+                else if(page === "Login") document.write("You Selected Login");
+                else if(page === "Links") document.write("You Selected Links");
+                document.write("<br>");
+                switch(page){
+                    case "News":
+                        document.write("You Selected News");
+                        break;
+                    case "ABout":
+                        document.write("You Selected About");
+                        break;
+                    default:
+                        document.write("Unrecognized Selection");
+                        break;
+                 
+                 
+                
+                
+                haystack = new Array();
+                haystack[16] = "Naddle";
+                
+                for(j = 0 ; j < 20 ; ++j) {
+                    if (haystack[j] === "Naddle") {
+                        document.write("Found at Location " + j);
+                        break
+                    }
+                    else {
+                        document.write(j + ", ");
+                    }
+                  }
+                  
+                  n = 3.1415927;
+                  i = parseInt(n);
+                  document.write(i);
+                }
+                
+             </script>
+            <noscript>
+                You Browser does not support or disabled the javascript
+            </noscript>
+           
+        </div>
+    </body>
+</html>
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+   
+                
+           
